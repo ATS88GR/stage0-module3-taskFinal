f43d1bf7b7c017734e6c43077b1caf323d08f26b
ATS88GR <turok.artyom@gmail.com>
2023-11-14 18:09:49 +0300

    add m3, tFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        boolean isZero = (number & 127) == 0;
+        boolean result = (((number >> 31) & 1) == 1) || isZero;
+        System.out.println(!result);
     }
 }
